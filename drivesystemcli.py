564718a7ac542a04c606c1cd9f64fa0f1a1e6c20
Patrick MacGregor <patrick.macgregor@cern.ch>
2025-06-17 12:46:08 +0200

    Added a bit more CLI functionality

--- drivesystemcli.py
+++ drivesystemcli.py
@@ -1,22 +1,26 @@
+import prompt_toolkit.patch_stdout
 import drivesystemlib as dslib
+import prompt_toolkit
 
 # TODO MAKE MORE SOPHISTICATED WITH CURSES?
 def cli_loop():
     drivesystem = dslib.DriveSystem.get_instance()
+    prompt_session = prompt_toolkit.PromptSession("DriveSystem >>> ")
     
     try:
-        while True:
-            # Ask for input
-            cmd = str(input("Input command: "))
-            
-            # Check commands
-            if cmd == "quit" or cmd == "q":
-                break
-            
-            # Send command
-            output = drivesystem.execute_command( f"{cmd}\r" )
-            
-            # Print return value
-            print(output)
-    except KeyboardInterrupt:
+        with prompt_toolkit.patch_stdout.patch_stdout():
+            while True:
+                # Ask for input
+                cmd = prompt_session.prompt()
+                
+                # Check commands
+                if cmd == "quit" or cmd == "q":
+                    break
+                
+                # Send command
+                output = drivesystem.execute_command( f"{cmd}\r" )
+                
+                # Print return value
+                print(f"{cmd} : AXIS {output[0]} -> {output[1]}")
+    except (EOFError, KeyboardInterrupt):
         print("")
